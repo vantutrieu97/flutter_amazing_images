5ea870971c1e791f7e4af8801cb3053616251727
vantutrieu97 <vantutrieu97@gmail.com>
2020-04-08 04:42:41 +0700

    Add home page

---
+++ lib/mvp/presenters/home_presenter.dart
@@ -0,0 +1,8 @@
+import 'package:flutteramazingiamges/mvp/base_page_presenter.dart';
+import 'package:flutteramazingiamges/ui/pages/index.dart';
+
+class HomePresenter extends BasePagePresenter<HomePageState>{
+  void changeTab(){
+
+  }
+}
---
+++ lib/ui/pages/home_page.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutteramazingiamges/constants/index.dart';
+import 'package:flutteramazingiamges/mvp/base_state.dart';
+import 'package:flutteramazingiamges/mvp/presenters/index.dart';
+import 'package:flutteramazingiamges/provider/index.dart';
+import 'package:provider/provider.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  HomePageState createState() => HomePageState();
+}
+
+class HomePageState extends BaseState<HomePage, HomePresenter> {
+  final List<Widget> bottomTab = [
+    SvgPicture.asset('assets/icons/home-outline.svg'),
+    SvgPicture.asset('assets/icons/compass-outline.svg'),
+    SvgPicture.asset('assets/icons/upload-outline.svg'),
+    SvgPicture.asset('assets/icons/account-outline.svg'),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    print('<> HomePage :<>--------<> build');
+    final provider = Provider.of<HomeProvider>(context);
+    int currentTab = provider.currentTab;
+    return Scaffold(
+      body: Container(
+        constraints: BoxConstraints.expand(),
+        color: Colors.white,
+        child: PageView(
+          children: <Widget>[
+            ProviderSetup.instance().secondPage(),
+            ProviderSetup.instance().secondPage(),
+            ProviderSetup.instance().secondPage(),
+            ProviderSetup.instance().secondPage(),
+          ],
+        ),
+      ),
+      bottomNavigationBar: Container(
+        height: 44,
+        color: Colors.white,
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            MaterialButton(
+              onPressed: () {
+                provider.changeTab(0);
+              },
+              child: SvgPicture.asset(
+                'assets/icons/home-outline.svg',
+                color: currentTab == 0
+                    ? DEFAULT_SELECTED_ICON
+                    : DEFAULT_UN_SELECTED_ICON,
+              ),
+            ),
+            MaterialButton(
+              onPressed: () {
+                provider.changeTab(1);
+              },
+              child: SvgPicture.asset(
+                'assets/icons/compass-outline.svg',
+                color: currentTab == 1
+                    ? DEFAULT_SELECTED_ICON
+                    : DEFAULT_UN_SELECTED_ICON,
+              ),
+            ),
+            MaterialButton(
+              onPressed: () {
+                provider.changeTab(2);
+              },
+              child: SvgPicture.asset(
+                'assets/icons/upload-outline.svg',
+                color: currentTab == 2
+                    ? DEFAULT_SELECTED_ICON
+                    : DEFAULT_UN_SELECTED_ICON,
+              ),
+            ),
+            MaterialButton(
+              onPressed: () {
+                provider.changeTab(3);
+              },
+              child: SvgPicture.asset(
+                'assets/icons/account-outline.svg',
+                color: currentTab == 3
+                    ? DEFAULT_SELECTED_ICON
+                    : DEFAULT_UN_SELECTED_ICON,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  HomePresenter createPresenter() {
+    return HomePresenter();
+  }
+}
