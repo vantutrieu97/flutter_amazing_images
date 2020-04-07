6bfc6a3917ea564dbeda6c018d5ed56a3a85bd24
vantutrieu97 <vantutrieu97@gmail.com>
2020-04-08 04:41:43 +0700

    Update color

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -1,14 +1,7 @@
 import 'package:flutter/material.dart';
 
-const Color DEFAULT_PINK = const Color(0xFFFF2872);
-const Color DEFAULT_WHITE_BG = const Color(0xFFF9FAFD);
-const Color FACEBOOK_BLUE = const Color(0xFF3B5999);
-const Color DEFAULT_TEXT_GREY = const Color(0xFF6D7EAC);
-const Color DEFAULT_BORDER = const Color(0xFFE8E8E8);
-const Color DEFAULT_HINT = const Color(0xFF6E7FAA);
-const Color DEFAULT_TEXT = const Color(0xFF222455);
-const Color DEFAULT_GREEN = const Color(0xFF4CD964);
-const Color DEFAULT_RED = const Color(0xFFEA172F);
+const Color DEFAULT_UN_SELECTED_ICON = const Color(0xff9E9E9E);
+const Color DEFAULT_SELECTED_ICON = const Color(0xFF212121);
 
 const GRADIENTS = const [
   [
