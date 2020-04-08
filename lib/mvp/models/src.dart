776072dee2c793b12fe452469e2ae38666e8cfb9
vantutrieu97 <vantutrieu97@gmail.com>
2020-04-09 00:09:20 +0700

    Add Src model

---
+++ lib/mvp/models/src.dart
@@ -0,0 +1,49 @@
+import 'dart:convert';
+
+class Src {
+  String original;
+  String large2X;
+  String large;
+  String medium;
+  String small;
+  String portrait;
+  String landscape;
+  String tiny;
+
+  Src({
+    this.original,
+    this.large2X,
+    this.large,
+    this.medium,
+    this.small,
+    this.portrait,
+    this.landscape,
+    this.tiny,
+  });
+
+  factory Src.fromRawJson(String str) => Src.fromJson(json.decode(str));
+
+  String toRawJson() => json.encode(toJson());
+
+  factory Src.fromJson(Map<String, dynamic> json) => Src(
+    original: json["original"],
+    large2X: json["large2x"],
+    large: json["large"],
+    medium: json["medium"],
+    small: json["small"],
+    portrait: json["portrait"],
+    landscape: json["landscape"],
+    tiny: json["tiny"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "original": original,
+    "large2x": large2X,
+    "large": large,
+    "medium": medium,
+    "small": small,
+    "portrait": portrait,
+    "landscape": landscape,
+    "tiny": tiny,
+  };
+}
